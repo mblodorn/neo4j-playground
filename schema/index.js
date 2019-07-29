e79e046c24bd682036c4784907e8aa6e5e679fb0
Matheus Blödorn <blodorn.matheus@gmail.com>
2019-07-29 18:09:29 -0300

    add uuid

--- schema/index.js
+++ schema/index.js
@@ -1,12 +1,21 @@
-const { makeAugmentedSchema } = require("neo4j-graphql-js");
+const { makeAugmentedSchema, neo4jgraphql } = require("neo4j-graphql-js");
 
 const typeDefs = /* GraphQL */ `
   type Person {
+    uuid: ID!
     name: String!
     email: String!
     relationships: [Person] @relation(name: "RELATIONSHIP", direction: IN)
   }
 `;
+
+const resolvers = {
+  Mutation: {
+    CreatePerson(object, params, ctx, resolveInfo) {
+      return neo4jgraphql(object, params, ctx, resolveInfo);
+    }
+  }
+};
 
 const schema = makeAugmentedSchema({ typeDefs });
 
