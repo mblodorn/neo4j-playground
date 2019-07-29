aadc74b742021597006035469f7a0be954d0156e
Matheus Blödorn <blodorn.matheus@gmail.com>
2019-07-29 17:44:20 -0300

    query person

--- schema/index.js
+++ schema/index.js
@@ -3,6 +3,8 @@ const { makeAugmentedSchema } = require("neo4j-graphql-js");
 const typeDefs = /* GraphQL */ `
   type Person {
     name: String!
+    email: String!
+    relationships: [Person] @relation(name: "RELATIONSHIP", direction: IN)
   }
 `;
 
